1b96a074a216ff31d39a8370f9e448d0a42ec3b6
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-10-04 02:41:41 +0900

    Add tests loading multiple files

--- main_test.go
+++ main_test.go
@@ -53,6 +53,60 @@ func TestSingleFileWithMultipleYAMLs(t *testing.T) {
 	}
 }
 
+func TestMultipleFilesWithSingleYAML(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go", "--", "fixtures/single.yaml", "fixtures/single.yaml", "fixtures/single.yaml")
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	if err := cmd.Run(); err != nil {
+		t.Errorf("failed: %v", err)
+	}
+
+	expected := `{"foo":"FOO"}
+{"foo":"FOO"}
+{"foo":"FOO"}
+`
+	if stdout.String() != expected {
+		t.Errorf("stdout doesn't match\nExpected:\n%s\nActual:\n%s", expected, stdout.String())
+	}
+
+	if stderr.String() != "" {
+		t.Errorf("stderr should be empty:\n%s", stderr.String())
+	}
+}
+
+func TestMultipleFilesWithMultipleYAMLs(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go", "--", "fixtures/multiple.yaml", "fixtures/multiple.yaml", "fixtures/multiple.yaml")
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	if err := cmd.Run(); err != nil {
+		t.Errorf("failed: %v", err)
+	}
+
+	expected := `{"foo":"FOO"}
+{"bar":"BAR"}
+{"baz":"BAZ"}
+{"foo":"FOO"}
+{"bar":"BAR"}
+{"baz":"BAZ"}
+{"foo":"FOO"}
+{"bar":"BAR"}
+{"baz":"BAZ"}
+`
+	if stdout.String() != expected {
+		t.Errorf("stdout doesn't match\nExpected:\n%s\nActual:\n%s", expected, stdout.String())
+	}
+
+	if stderr.String() != "" {
+		t.Errorf("stderr should be empty:\n%s", stderr.String())
+	}
+}
+
 func TestVersion(t *testing.T) {
 	cmd := exec.Command("go", "run", "-ldflags", "-X main.version=1.2.3 -X main.gitCommit=deadbeef", "main.go", "--version")
 	stdout := &bytes.Buffer{}

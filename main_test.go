7ecf935bcfcbee70f7bdf092f3bf1e6f2bfc1480
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-10-04 03:01:24 +0900

    Add tests reading from stdin

--- main_test.go
+++ main_test.go
@@ -3,9 +3,70 @@ package main
 import (
 	"bytes"
 	"errors"
+	"os"
 	"os/exec"
 	"testing"
 )
+
+func TestSingleYAMLFromStdin(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go")
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	file, err := os.Open("fixtures/single.yaml")
+	if err != nil {
+		t.Errorf("failed: %v", err)
+	}
+
+	cmd.Stdin = file
+
+	if err := cmd.Run(); err != nil {
+		t.Errorf("failed: %v", err)
+	}
+
+	expected := `{"foo":"FOO"}
+`
+	if stdout.String() != expected {
+		t.Errorf("stdout doesn't match\nExpected:\n%s\nActual:\n%s", expected, stdout.String())
+	}
+
+	if stderr.String() != "" {
+		t.Errorf("stderr should be empty:\n%s", stderr.String())
+	}
+}
+
+func TestMultipleYAMLsFromStdin(t *testing.T) {
+	cmd := exec.Command("go", "run", "main.go")
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	file, err := os.Open("fixtures/multiple.yaml")
+	if err != nil {
+		t.Errorf("failed: %v", err)
+	}
+
+	cmd.Stdin = file
+
+	if err := cmd.Run(); err != nil {
+		t.Errorf("failed: %v", err)
+	}
+
+	expected := `{"foo":"FOO"}
+{"bar":"BAR"}
+{"baz":"BAZ"}
+`
+	if stdout.String() != expected {
+		t.Errorf("stdout doesn't match\nExpected:\n%s\nActual:\n%s", expected, stdout.String())
+	}
+
+	if stderr.String() != "" {
+		t.Errorf("stderr should be empty:\n%s", stderr.String())
+	}
+}
 
 func TestSingleFileWithSingleYAML(t *testing.T) {
 	cmd := exec.Command("go", "run", "main.go", "--", "fixtures/single.yaml")
